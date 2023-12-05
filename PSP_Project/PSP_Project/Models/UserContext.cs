7621da557b7a3ffa090fed85d32d28427c2784fb
Иван Шлома <ivanbelobrovik@yandex.ru>
2023-12-05 20:37:27 +0300

    GitProject

---
+++ PSP_Project/PSP_Project/Models/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PSP_Project.Models {
+    public class UserContext : DbContext {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options) {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ PSP_Project/PSP_Project/ViewModels/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PSP_Project.ViewModels {
+    public class LoginModel {
+        [Required(ErrorMessage = "Не указан Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
